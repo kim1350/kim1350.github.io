50a2e7fc829d219000416b0425e6497e3c2c0f62
kim1350 <70932884+kim1350@users.noreply.github.com>
2022-04-19 09:57:04 +0000

    Deploying to gh-pages from @ kim1350/kim1350.github.io@626db50a094866babaa61f5992b16b166d2bcaa0 🚀

---
+++ _next/static/4NXIJUe2yyN2bXBolAP0P/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9ccb59c6c1b66422.js"],"/_error":["static/chunks/pages/_error-e526a695839b5a36.js"],"/about":["static/chunks/pages/about-2665082ae1611364.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
